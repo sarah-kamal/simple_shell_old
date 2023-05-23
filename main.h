0b943d518466d4cf2916d98001bb8cd1b20200ce
sarah-kamal <118070344+sarah-kamal@users.noreply.github.com>
2023-05-24 01:37:43 +0300

    Update main.h

--- main.h
+++ main.h
@@ -13,8 +13,15 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <signal.h>
-
+int printenv(char *arr);
 char *create_buff();
 void sighandler(int sig_num);
-
+char* customstrtok(char *buff,char );
+void putword(char *word);
+void stringcopy(char *dest, const char *src);
+void stringcat(char *dest, const char *src);
+int stringcompare(const char *str1, const char *str2);
+int exitcode(char *arr, char *code);
+void handle_error(char *fname, int linecount, int n);
+void tostring(char str[], int num);
 #endif
