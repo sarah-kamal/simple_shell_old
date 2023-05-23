2d17ee6c8678f79771b22fe22c0c6b1d8d9f3508
sarah-kamal <118070344+sarah-kamal@users.noreply.github.com>
2023-05-24 01:38:29 +0300

    Update hand_in.c

--- hand_in.c
+++ hand_in.c
@@ -6,11 +6,54 @@ char *create_buff()
 
 	buffer = malloc(sizeof(char) * 1024);
 	if (!buffer)
+	{
+		free(buffer);
 		exit(90);
+	}
 	return (buffer);
 }
 
 void sighandler(int signum)
 {
+	signal(SIGINT, sighandler);
 	printf("Stopped: %d\n", signum);
+}
+
+char * customstrtok(char * str, char del)
+{
+        char *token;
+        static char *nexttoken;
+        if (str != NULL)
+        {
+                nexttoken = str;
+        }
+        else if (nexttoken == NULL)
+        {
+                return (NULL);
+        }
+        while (*nexttoken && *nexttoken == del)
+                nexttoken++;
+        token = nexttoken;
+        while (*nexttoken && *nexttoken != del)
+                nexttoken++;
+        if (*nexttoken)
+        {
+                *nexttoken ='\0';
+                nexttoken++;
+        }
+        else
+        {
+                nexttoken = NULL;
+        }
+        /*printf("token %s\n", token);
+        fflush(stdout);*/
+        return (token);
+}
+void putword(char *word)
+{
+	int i;
+	for (i = 0; word[i] != '\0'; i++);
+	if (write(STDOUT_FILENO, word, i) == -1)
+		exit(90);
+	fflush(stdout);
 }
